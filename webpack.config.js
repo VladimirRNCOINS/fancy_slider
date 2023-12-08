3cd9908f9789738f729d3b756e856efe9d8f093f
VladimirRNCOINS <v.shabalov@rncoins.ru>
2023-12-08 18:58:42 +0300

    выгрузка css файлов в настройках webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RemovePlugin = require("remove-files-webpack-plugin");
+
+let nowDate = Date.now();
+
 module.exports = {
-    entry: './public/index.js',
+    entry: {
+        js: './public/index.js',
+        style: './assets/css/zoomNdrag.css',
+    },
     output: {
         path: __dirname,
-        filename: './dist/bundle_' + Date.now() + '.js',
+        filename: './dist/[name]_bundle_' + nowDate + '.js',
         library: 'zoomNdrag'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./dist/[name]_bundle_" + nowDate + ".css",
+        }),
+        new RemovePlugin ({
+            after: {
+                include: ["./dist/style_bundle_" + nowDate + ".js"]
+            }
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ],
     },
     mode: 'production'
 };
